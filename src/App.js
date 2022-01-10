dcf6f0968948c317af25ee9e349c154b2eeb90e2
Gavriil84 <gabwt.84@gmail.com>
2022-01-10 11:36:18 -0800

    add section for input & output

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import './App.css';
 
 function App() {
   return (
     <div>
+      <section id="input-form">
 
+      </section>
+      <section id="output-form"></section>
     </div>
   );
 }
