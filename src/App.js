5365ff223844ad54eaaeaa676a11df8ffa7544e7
Gavriil84 <gabwt.84@gmail.com>
2022-01-10 15:18:32 -0800

    add useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import UserInput from './components/NameList/UserInput/UserInput';
 
 import './App.css';
 
 function App() {
+  const [userInfo, setUserInfo] = useState([]);
+
+  const addUserHandler = (value) => {
+    setUserInfo([...userInfo, value]);
+  }
+
   return (
     <div>
       <section id="input-form">
-
+        <UserInput onAddUser={addUserHandler} />
       </section>
       <section id="output-form"></section>
     </div>
