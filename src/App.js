30c71774590fd1a5dc8b56b8cce7da9ac616971d
Gavriil84 <gabwt.84@gmail.com>
2022-01-11 22:28:15 -0800

    add user list component to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import UserInput from './components/NameList/UserInput/UserInput';
+import UserList from './components/NameList/UserOutput/UserList/UserList';
 
 import './App.css';
 
@@ -10,14 +11,18 @@ function App() {
     setUserInfo([...userInfo, value]);
   }
 
+  console.log(userInfo)
   return (
     <div>
       <section id="input-form">
         <UserInput onAddUser={addUserHandler} />
       </section>
-      <section id="output-form"></section>
+      <section id="output-form">
+        <UserList items={userInfo} />
+      </section>
     </div>
   );
+
 }
 
 export default App;
