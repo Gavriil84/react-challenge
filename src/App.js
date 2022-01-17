4be13063d27440d8ecabbcc836c3cf6cf00cfb02
Gavriil84 <gabwt.84@gmail.com>
2022-01-16 22:27:35 -0800

    delete code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,6 @@
-import React, { useState } from 'react';
-import UserInput from './components/NameList/UserInput/UserInput';
-import UserList from './components/NameList/UserOutput/UserList/UserList';
-
-import './App.css';
-
 function App() {
-  const [userInfo, setUserInfo] = useState([]);
-
-  const addUserHandler = (value) => {
-    setUserInfo([...userInfo, value]);
-  }
-
-  console.log(userInfo)
   return (
-    <div>
-      <section id="input-form">
-        <UserInput onAddUser={addUserHandler} />
-      </section>
-      <section id="output-form">
-        <UserList items={userInfo} />
-      </section>
-    </div>
+
   );
 
 }
